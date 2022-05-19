47fba31b39c9223ff2a9b6e5bd1084af40845e41
Marco Grassi <marco.gra@gmail.com>
2022-05-19 20:48:30 +0800

    new version with better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
+use anyhow::anyhow;
 use clap::{Arg, Command};
 use std::path::Path;
 use tx_custom_boot::generate_boot_dat;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let cmd = Command::new(env!("CARGO_PKG_NAME"))
-    .version(env!("CARGO_PKG_VERSION"))
-    .about(env!("CARGO_PKG_DESCRIPTION"))
-    .author(env!("CARGO_PKG_AUTHORS"))
-    .arg(Arg::new("payload").takes_value(true).required(true).help("The payload to be converted to boot.dat"))
-    .arg(Arg::new("output").takes_value(true).required(false).help("The output file"));
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .arg(
+            Arg::new("payload")
+                .takes_value(true)
+                .required(true)
+                .help("The payload to be converted to boot.dat"),
+        )
+        .arg(
+            Arg::new("output")
+                .takes_value(true)
+                .required(false)
+                .help("The output file"),
+        );
 
     let matches = cmd.get_matches();
 
@@ -17,20 +28,21 @@ fn main() {
 
     // Check if the input file exists
     if !Path::new(payload).exists() {
-        eprintln!("Input file does not exist.");
-        return;
+        return Err(anyhow!("Input file does not exist."));
     }
 
     // Check if the output file exists
     if Path::new(output).exists() {
-        eprintln!("Output file already exists, please remove it first.");
-        return;
+        return Err(anyhow!(
+            "Output file already exists, please remove it first."
+        ));
     }
 
     // Read the input file
-    let payload = std::fs::read(payload).expect("cannot read the input file");
-    let generated = generate_boot_dat(&payload);
+    let payload = std::fs::read(payload)?;
+    let generated = generate_boot_dat(&payload)?;
 
     // Write the output file
-    std::fs::write(output, generated).expect("cannot write the output file");
+    std::fs::write(output, generated)?;
+    Ok(())
 }
